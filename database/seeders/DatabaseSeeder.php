150aad3bf9e53e399328a4f8f93370230831c428
Mohamad Arif <125123688+mohamadarif03@users.noreply.github.com>
2026-08-25 09:45:57 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,7 +21,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'justin@gmail.com',
-            'password' => '123justin',
+            'password' => Hash::make('123justin'),
             'role' => 'admin'
         ]);
     }
